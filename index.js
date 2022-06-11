250c6cce63bf502951a2d51136b13cdff752032e
Mhamad <mouhamedhannan@gmail.com>
2022-06-11 16:27:21 +0300

    Working game up until 5 rounds, still needs some functions

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function rps() {
+
+let playerPoints = 0;
+let computerPoints = 0;
+
 //Create array with the available 'weapons'.
 
 let weapon = ['rock','paper','scissors'];
@@ -8,11 +13,37 @@ function computerPlay() {
 return weapon[Math.floor(Math.random()*weapon.length)];    
 };
 
-let computerSelection = computerPlay();
+
 
 //Get player's selection and store in a variable
 
-let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+
+
+for (let i = 0; i < 5; i++) {
+
+  let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+  let computerSelection = computerPlay();
+  //display the choices made by the computer and the player and play a round.
+
+  console.log('You chose: ' + playerSelection);
+  console.log('The Machine chose: ' + computerSelection);
+  let play = round(playerSelection, computerSelection);
+
+  if(play === "win"){
+    playerPoints++;
+    console.log('Player: ' + playerPoints + ' vs' + 'The Machine: ' + computerPoints);
+  } else if(play === "loss"){
+    computerPoints++;
+    console.log('Player: ' + playerPoints + ' vs' + 'The Machine: ' + computerPoints);
+  }
+    else {
+      console.log('Player: ' + playerPoints + ' vs ' + 'The Machine: ' + computerPoints);
+    }
+
+}
+}
+
+
 
 //Function for 1 round between player and computer. Should include the if cases with the needed printout. Function must have 
 //two parameters, the players selection and the computers selection. 
@@ -23,37 +54,40 @@ function round(playerSelection, computerSelection) {
 
     if (playerSelection == 'rock') {
       if (computerSelection == 'scissors') {
-        alert('Player wins.');
-      } else if (computerSelection == 'The Machine wins.') {
+        console.log('Player wins.');
+        return 'win';
+      } else if (computerSelection == 'paper') {
         console.log('The Machine wins.');
+        return 'loss';
       } else {
         console.log('It\'s a tie.');
+        return 'tie';
       } 
     }
     if (playerSelection == 'paper') {
       if (computerSelection == 'rock') {
         console.log('Player wins.');
+        return 'win';
       } else if (computerSelection == 'scissors') {
         console.log('The Machine wins.');
+        return 'loss';
       } else {
         console.log('It\'s a tie.');
+        return 'tie';
       } 
     }
     if (playerSelection == 'scissors') {
       if (computerSelection == 'rock') {
         console.log('The Machine wins.');
+        return 'loss';
       } else if (computerSelection == 'paper') {
         console.log('Player wins.');
+        return 'win';
       } else {
         console.log('It\'s a tie.');
+        return 'tie';
       } 
     }
   }
 
-//display the choices made by the computer and the player and play a round.
-
-console.log('You chose: ' + playerSelection);
-console.log('The Machine chose: ' + computerPlay());
-
-round(playerSelection, computerSelection);
-
+  rps();
