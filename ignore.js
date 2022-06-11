48b29d8d4ffef4efa11189e5e79b27d69f1109b4
Mhamad <mouhamedhannan@gmail.com>
2022-06-11 12:20:08 +0300

    Wrote function for one round, not working properly

---
+++ ignore.js
@@ -0,0 +1,55 @@
+if( (playerSelection === "rock" && computerSelection === "scissor") ){
+  console.log('Player won this round.')
+}
+
+else if( (playerSelection === "scissor" && computerSelection === "paper") ){
+  console.log('Player won this round.')
+}
+
+else if( (playerSelection === "paper" && computerSelection === "rock") ){
+  console.log('Player won this round.')
+}
+
+else if( (playerSelection === "scissor" && computerSelection === "rock") ){
+  console.log('The Machine won this round.')
+}
+
+else if( (playerSelection === "paper" && computerSelection === "scissor") ){
+  console.log('The Machine won this round.')
+}
+
+else if( (playerSelection === "rock" && computerSelection === "paper") ){
+  console.log('The Machine won this round.')
+}
+
+
+
+//if (playerSelection === 'rock') {
+//  if(computerSelection === 'rock') {
+//    console.log('It\'s a tie.');
+//  }
+//  else if(computerSelection === 'paper') {
+//    console.log('The Machine won this round.');
+//  }
+//  else if(computerSelection === 'scissor') {
+//    console.log('Player won this round.');
+//  }
+//}
+
+//if( ((playerSelection === "rock") && (computerSelection === "scissor")) || 
+//    ((playerSelection === "scissor") && (computerSelection === "paper")) ||
+//    ((playerSelection === "paper") && (computerSelection === "rock")) ) {
+//  console.log('Player won this round.')
+//}
+//
+//if ( ((playerSelection === "scissor") && (computerSelection === "rock")) || 
+//          ((playerSelection === "paper") && (computerSelection === "scissor")) || 
+//          ((playerSelection === "rock") && (computerSelection === "paper")) ) {
+// console.log('The Machine won this round.')
+//}
+//
+//if(  ((playerSelection === "rock") && (computerSelection === "rock")) || 
+//          ((playerSelection === "scissor") && (computerSelection === "scissor")) || 
+//          ((playerSelection === "paper") && (computerSelection === "paper")) ) {
+// console.log('It\'s a tie.')
+//}
---
+++ index.js
@@ -0,0 +1,64 @@
+//Create array with the available 'weapons'.
+
+let weapon = ['rock','paper','scissor'];
+
+//Function to return a weapon selected at random by the computer.
+
+function computerPlay() {
+return weapon[Math.floor(Math.random()*weapon.length)];    
+};
+
+let computerSelection = computerPlay();
+
+//Get player's selection and store in a variable
+
+let playerSelection = prompt('What will be your weapon of choice');
+
+//display the choices made by the computer and the player.
+
+
+
+
+//Function for 1 round between player and computer. Should include the if cases with the needed printout. Function must have 
+//two parameters, the players selection and the computers selection. 
+//Run conditional statements to see who wins the round
+
+
+function round(playerSelection, computerSelection) {
+
+    if (playerSelection == 'rock') {
+      if (computerSelection == 'scissors') {
+        console.log('Player wins.');
+      } else if (computerSelection == 'The Machine wins.') {
+        console.log('The Machine wins.');
+      } else if (computerSelection == 'rock') {
+        console.log('It\'s a tie.');
+      } 
+    }
+    if (playerSelection == 'paper') {
+      if (computerSelection == 'rock') {
+        console.log('Player wins.');
+      } else if (computerSelection == 'scissors') {
+        console.log('The Machine wins.');
+      } else {
+        console.log('It\'s a tie.');
+      } 
+    }
+    if (playerSelection == 'scissors') {
+      if (computerSelection == 'rock') {
+        console.log('The Machine wins.');
+      } else if (computerSelection == 'paper') {
+        console.log('Player wins.');
+      } else {
+        console.log('It\'s a tie.');
+      } 
+    }
+  }
+
+
+
+console.log('You chose: ' + playerSelection);
+console.log('The Machine chose: ' + computerPlay());
+
+round(playerSelection, computerSelection);
+
