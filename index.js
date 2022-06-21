d656e3b063b24107925e6bec9acc7010701e3987
Mhamad <mouhamedhannan@gmail.com>
2022-06-21 13:30:28 +0300

    Buttons functioning, need to add condition and winner

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
-function rps() {
-
+const container = document.querySelector('#container');
+const btns = document.querySelectorAll('button');
+const scores = document.querySelector('#scores');
+const results = document.querySelector('#result');
 let playerPoints = 0;
 let computerPoints = 0;
 
-//Create array with the available 'weapons'.
-
-let weapon = ['rock','paper','scissors'];
-
-//Function to return a choice selected at random by the computer.
-
-function computerPlay() {
-return weapon[Math.floor(Math.random()*weapon.length)];    
-};
-
-//for loop to run for 5 rounds.
-for (let i = 0; i < 5; i++) {
-
-  //Get player's selection and computer's selection and store in a variable
-  let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-  let computerSelection = computerPlay();
-
-  //display the choices made by the computer and the player and play a round.
-  console.log('You chose: ' + playerSelection);
-  console.log('The Machine chose: ' + computerSelection);
-
-  let play = round(playerSelection, computerSelection);
-
-
-  //Conditional statements to add points to the winning party and display the current standings
-  if(play === "win"){
-    playerPoints++;
-    console.log('Player: ' + playerPoints + ' vs ' + 'The Machine: ' + computerPoints);
-  } else if(play === "loss"){
-    computerPoints++;
-    console.log('Player: ' + playerPoints + ' vs ' + 'The Machine: ' + computerPoints);
-  }
-    else {
-      console.log('Player: ' + playerPoints + ' vs ' + 'The Machine: ' + computerPoints);
-    }
-}
-
-if(playerPoints > computerPoints){
-  console.log('');
-  console.log("Congratulations!  You beat the computer " + playerPoints + " to " + computerPoints);
-} else {
-  console.log("Sorry!  You lost to the computer " + computerPoints + " to " + playerPoints);
-}
-
-}
-
-
+btns.forEach((button) => {
+  button.addEventListener('click', getPlayerChoice)
+  });
 
 //Function for 1 round between player and computer. Should include the if cases with the needed printout. Function must have 
 //two parameters, the players selection and the computers selection. 
@@ -60,40 +18,47 @@ function round(playerSelection, computerSelection) {
 
     if (playerSelection == 'rock') {
       if (computerSelection == 'scissors') {
-        console.log('Player wins.');
-        return 'win';
+        results.textContent = 'You win! Rock beats Scissors.'
+        ++playerPoints;
       } else if (computerSelection == 'paper') {
-        console.log('The Machine wins.');
-        return 'loss';
+        results.textContent = 'You lose:( Paper beats Rock.'
+        ++computerPoints;
       } else {
-        console.log('It\'s a tie.');
-        return 'tie';
+        results.textContent = 'It\'s a tie.'
       } 
     }
     if (playerSelection == 'paper') {
       if (computerSelection == 'rock') {
-        console.log('Player wins.');
-        return 'win';
+        results.textContent = 'You win! Paper beats Rock.'
+        ++playerPoints;
       } else if (computerSelection == 'scissors') {
-        console.log('The Machine wins.');
-        return 'loss';
+        results.textContent = 'You lose:( Scissors beats Paper.'
+        ++computerPoints;
       } else {
-        console.log('It\'s a tie.');
-        return 'tie';
+        results.textContent = 'It\'s a tie.'
       } 
     }
     if (playerSelection == 'scissors') {
       if (computerSelection == 'rock') {
-        console.log('The Machine wins.');
-        return 'loss';
+        results.textContent = 'You lose:( Rock beats Scissors.'
+        ++computerPoints; 
       } else if (computerSelection == 'paper') {
-        console.log('Player wins.');
-        return 'win';
+        results.textContent = 'You win! Scissors beats Paper.'
+        ++playerPoints;
       } else {
-        console.log('It\'s a tie.');
-        return 'tie';
+        results.textContent = 'It\'s a tie.'
       } 
     }
+     
+    scores.textContent = `You: ${playerPoints} The Machine: ${computerPoints}`
   }
 
-  rps();
+
+  function getPlayerChoice(e) {
+    let weapon = ['rock','paper','scissors'];
+    let playerSelection= (e.target.id);
+    let computerSelection = weapon[Math.floor(Math.random()*weapon.length)];
+    playerChoice = e.target.textContent;
+    round(playerSelection, computerSelection);
+  }
+
